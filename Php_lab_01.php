99286bddaae245f356adaa4d7dabd40c92046c94
tahajim <tahajimislam20@gmail.com>
2025-12-18 11:20:00 +0600

    Php lab 1

---
+++ Php_lab_01.php
@@ -0,0 +1,43 @@
+<?php
+
+$name = "Tahajim";
+
+$num01 = 20;
+
+$num02 = 5.5;
+
+$bool = true;
+
+$addition = $num01 + $num02;
+
+$subtraction = $num01 - $num02;
+
+$multiplication = $num01 * $num02;
+
+$division = $num01 / $num02;
+
+$sum = $num01 + $num02;
+
+echo "Addition: ", $addition, "<br>";
+
+echo "Subtraction: ", $subtraction, "<br>";
+
+echo "Multiplication: ", $multiplication, "<br>";
+
+echo "Division: ", $division, "<br>";
+
+print "Sum of Number 01 + Number 02: ";
+
+print $sum;
+
+print "<br>";
+
+var_dump($name);
+echo "<br>";
+var_dump($num01);
+echo "<br>";
+var_dump($num02);
+echo "<br>";
+var_dump($bool);
+
+?>
